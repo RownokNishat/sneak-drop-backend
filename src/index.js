182a9cc14fc2b874d9c2520246d4da42d60d56e8
RownokNishat <rownokjahan900@gmail.com>
2026-04-30 18:57:28 +0600

    cors error

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const io = initializeSocket(server);
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
-  }),
+  })
 );
 app.use(express.json());
 
