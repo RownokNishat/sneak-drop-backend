fdaeef96a5db4ba8b48b338d2ef36c8007e12964
RownokNishat <rownokjahan900@gmail.com>
2026-04-30 19:07:09 +0600

    fix: cors error

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ const io = initializeSocket(server);
 // Middleware
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    origin: [
+      process.env.FRONTEND_URL,
+      "https://sneak-drop-frontend.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   }),
 );
